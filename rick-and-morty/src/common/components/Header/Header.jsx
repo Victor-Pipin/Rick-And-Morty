225a8abdb5785df82c222d4e1df75c68cc8701a6
Victor Pipin <parabletraveller@gmail.com>
2025-03-16 19:40:22 +0300

    Feat: In the Header component, add a <nav> element with five <NavLink> links

--- rick-and-morty/src/common/components/Header/Header.jsx
+++ rick-and-morty/src/common/components/Header/Header.jsx
@@ -2,15 +2,23 @@
     // В атрибуте src вместо кавычек использую красивые скобки, синтаксис jsx, т.к. кавычки это строка,
     // а скобки позволяют работать с import, сущностями нетипичными для HTML, причём адрес указан в импорте,
     // в атрибут src передаю только ссылку(logo) на адрес
+        // В теге nav вместо элементов <a>, ссылки <NavLink> из либы react-router, не перезагружают страницу при переходе
+import { NavLink } from "react-router"
 import logo from "../../../assets/img/logo.png"
 
 // Компонент общего Хедера для всего приложения, отображается на всех страницах
 export const Header = () => {
     return (
-        <div>
-            <div>
-                <img src={logo} alt="logotype" /> 
-            </div>
-        </div>
+        <header>
+            <nav>
+                <NavLink to={"/"}>
+                    <img src={logo} alt="logotype" />
+                </NavLink>
+                <NavLink to={"/"}>Home</NavLink>
+                <NavLink to={"/characters"}>Characters</NavLink>
+                <NavLink to={"/locations"}>Locations</NavLink>
+                <NavLink to={"/episodes"}>Episodes</NavLink>
+            </nav>
+        </header>
     )
 }
