bce883a27c195f593a8a79f0fbe44bf31e7a90c2
Victor Pipin <parabletraveller@gmail.com>
2025-03-16 12:38:14 +0300

    Feat: Configure routing for application pages using Routs, Rout components

--- rick-and-morty/src/main.jsx
+++ rick-and-morty/src/main.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter } from "react-router"
 import "./index.css"
 import App from "./App.jsx"
 
+// Для управления маршрутизацией оборачиваю всё приложение <App /> в <BrowserRouter>, создаёт контекст для всех сыновьих элементов
 createRoot(document.getElementById("root")).render(
     <BrowserRouter>
         <App />
--- rick-and-morty/src/pages/EpisodePage/EpisodePage.jsx
+++ rick-and-morty/src/pages/EpisodePage/EpisodePage.jsx
@@ -1,5 +1,5 @@
 // Компонент страницы со списком, количеством персонажей, названием и датой выхода эпизодов
-export const EpisodesPage = () => {
+export const EpisodePage = () => {
     return (
         <div>
             <h1>EpisodePage</h1>
