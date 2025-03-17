f2987d0b79c4776bf2a79254e9252f454246d464
Victor Pipin <parabletraveller@gmail.com>
2025-03-17 22:44:07 +0300

    Feat: Add styles to homepage component for h1 element using modular css

--- rick-and-morty/src/pages/HomePage/HomePage.jsx
+++ rick-and-morty/src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,6 @@
-import { NavLink } from "react-router"
+import { NavLink } from "react-router";
+// Подключение модульного css, название любое, кейс этого подключения - s(первая буква style), далее применяю этот модуль к h1
+import s from "./HomePage.module.css";
 
 // Компонент домашней страницы HomePage()
     // Три ссылки <NavLink to={"/locations"}>Locations</NavLink> для переходов на соответствующие страницы, 
@@ -6,7 +8,7 @@ import { NavLink } from "react-router"
 export const HomePage = () => {
     return (
         <div>
-            <h1>The Rick and Morty</h1>
+            <h1 className={s.title}>The Rick and Morty</h1>
             <nav>
                 <NavLink to={"/characters"}>Characters</NavLink>
                 <NavLink to={"/locations"}>Locations</NavLink>
