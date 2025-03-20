b1478c9d596f013b161ad58466ee89ff767b3985
Victor Pipin <parabletraveller@gmail.com>
2025-03-20 22:39:44 +0300

    Feat: Add a background image to the full screen of the homepage

--- rick-and-morty/src/pages/HomePage/HomePage.jsx
+++ rick-and-morty/src/pages/HomePage/HomePage.jsx
@@ -10,7 +10,7 @@ import s from "./HomePage.module.css";
         // из css-модуля для домашней страницы ${s.title}, в которых лежит нужный размер шрифта, этот паттерн не допускает бойлерплэйта
 export const HomePage = () => {
     return (
-        <div>
+        <div className={s.mainWrapper}>
             <h1 className={`pageTitle ${s.title}`}>The Rick and Morty</h1>
             <nav className={s.linkWrapper}>
                 <NavLink to={"/characters"} className={"linkButton"}>Characters</NavLink>
